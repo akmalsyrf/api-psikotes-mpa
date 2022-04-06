86e13ea411845777135493ae55aceb2432994796
akmalsyrf <dev.akmal69@gmail.com>
2022-04-06 15:47:46 +0700

    06-04-2022

--- app/psikotest/controller.js
+++ app/psikotest/controller.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const { Psikotest, Speed_prex_grade, Accuracy_prex_grade, Access_code, Accumulation_speed, Exam, Question, Option, Answer, Student_answer } = require("../../models");
+const { Psikotest, Speed_prex_grade, Accuracy_prex_grade, Endurance_prex_grade, Access_code, Accumulation_speed, Accumulation_accuracy, Accumulation_endurance, Question, Option, Answer, Student_answer } = require("../../models");
 
 exports.getAllPsikotest = async (req, res) => {
 	try {
@@ -41,7 +41,7 @@ exports.speedTest = async (req, res) => {
 				const qty = data.length;
 				const user_id = Number(req.query.user);
 
-				data.forEach(async (item) => {
+				data.forEach(async (item, i) => {
 					const exam_id = item.exam_id;
 
 					//get qty of question
@@ -49,7 +49,9 @@ exports.speedTest = async (req, res) => {
 					const getQuestionInExam = await axios.get(url);
 					const qty_question = getQuestionInExam.data.data.questions.length;
 
-					await Speed_prex_grade.create({ user_id, psikotest_id: psikotest.id, exam_id: Number(exam_id), qty, qty_question });
+					if (i === data.length - 1) {
+						await Speed_prex_grade.create({ user_id, psikotest_id: psikotest.id, exam_id: Number(exam_id), qty, qty_question });
+					}
 				});
 				res.status(200).json({
 					status: "success",
@@ -157,7 +159,7 @@ exports.accuracyTest = async (req, res) => {
 
 				let qty = 0;
 				let qty_question;
-				data.forEach(async (item) => {
+				await data.forEach(async (item, i) => {
 					const exam_id = item.exam_id;
 
 					//get qty of question
@@ -170,33 +172,161 @@ exports.accuracyTest = async (req, res) => {
 						where: {
 							id: item.question_id,
 						},
-						include: [
-							{
-								model: Option,
-							},
-							{
-								model: Answer,
-							},
-						],
+						include: [{ model: Option }, { model: Answer }],
 					});
 					const options = JSON.parse(JSON.stringify(question.Options));
 					const answer = JSON.parse(JSON.stringify(question.Answer));
 
+					//check answer
 					if (options[item.answer].id === Number(answer.option_id)) {
-						qty++;
+						qty += 1;
 					}
-					await Student_answer.create({ user_id, exam_id: Number(exam_id), question_id: item.question_id, option_id: Number(item.answer), qty_question });
+
+					//store data student answer
+					await Student_answer.create({ user_id, exam_id: Number(exam_id), question_id: item.question_id, option_id: Number(item.answer) });
+
+					//store data accuracy test in last looping data
+					if (i === data.length - 1) {
+						await Accuracy_prex_grade.create({ user_id, psikotest_id: psikotest.id, exam_id: Number(exam_id), qty, qty_question });
+					}
 				});
-				// await Accuracy_prex_grade.create({ user_id, psikotest_id: psikotest.id, exam_id: Number(exam_id), qty, qty_question });
 				res.status(200).json({
 					status: "success",
 					message: "Accuracy test success",
+				});
+			} else {
+				res.status(403).json({
+					status: "failed",
+					message: "access_code or test_code isn't valid",
+				});
+			}
+		} else {
+			res.status(403).json({
+				status: "failed",
+				message: "You must provide a test_code, user_id and access_code",
+			});
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			status: "failed",
+			message: "Server error",
+		});
+	}
+};
+
+exports.accuracyTestAccumulation = async (req, res) => {
+	try {
+		if (req.body.test_code && req.body.access_code && req.body.user_id) {
+			const { test_code, access_code, user_id } = req.body;
+
+			//check existence test_code and access_code
+			const psikotest = await Psikotest.findOne({
+				where: {
+					test_code: test_code,
+				},
+				include: {
+					model: Access_code,
+					where: {
+						access_code: access_code,
+					},
+				},
+			});
+			if (psikotest !== null) {
+				const psikotest_id = psikotest.id;
+
+				const accuracyTest = await Accuracy_prex_grade.findAll({
+					where: {
+						user_id: user_id,
+						psikotest_id: psikotest_id,
+					},
+				});
+
+				let qty = [];
+				let qty_question = [];
+				await accuracyTest.forEach((item) => {
+					qty.push(item.qty);
+					qty_question.push(item.qty_question);
+				});
+				qty = qty.reduce((a, b) => a + b, 0);
+				qty_question = qty_question.reduce((a, b) => a + b, 0);
+
+				const accumulation_accuracy = await Accumulation_accuracy.create({ user_id, psikotest_id, qty, qty_question });
+				res.status(200).json({
+					status: "success",
+					data: {
+						accumulation_accuracy,
+					},
+				});
+			}
+		} else {
+			res.status(403).json({
+				status: "failed",
+				message: "You must provide a test_code, user_id and access_code",
+			});
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			status: "failed",
+			message: "Server error",
+		});
+	}
+};
+
+exports.enduranceTest = async (req, res) => {
+	try {
+		if (req.body.test_code && req.body.access_code) {
+			const { test_code, access_code } = req.body;
+
+			//check existence test_code and access_code
+			const psikotest = await Psikotest.findOne({
+				where: {
+					test_code: test_code,
+				},
+				include: {
+					model: Access_code,
+					where: {
+						access_code: access_code,
+					},
+				},
+			});
+
+			if (psikotest !== null) {
+				const user_id = Number(req.query.user);
+				const psikotest_id = psikotest.id;
+				const accuracyTest = await Accuracy_prex_grade.findAll({
+					order: [["id", "ASC"]],
+					where: {
+						user_id,
+						psikotest_id,
+					},
+				});
+
+				let from_exam_id;
+				let to_exam_id;
+				let percentage_progress;
+				let difference_qty;
+
+				for (let i = 1; i < accuracyTest.length; i++) {
+					from_exam_id = accuracyTest[i - 1].exam_id;
+					to_exam_id = accuracyTest[i].exam_id;
+					difference_qty = accuracyTest[i].qty - accuracyTest[i - 1].qty;
+					percentage_progress = (difference_qty / accuracyTest[i - 1].qty) * 100;
+
+					if (i >= 1) {
+						await Endurance_prex_grade.create({ user_id, psikotest_id, from_exam_id, to_exam_id, percentage_progress, difference_qty });
+					}
+				}
+
+				res.status(200).json({
+					status: "success",
+					message: "Endurance test success",
 					// data: {
-					// 	qty,
-					// 	qty_question,
-					// 	psikotest_id: psikotest.id,
-					// 	exam_id: Number(exam_id),
-					// 	user_id,
+					// 	from_exam_id,
+					// 	to_exam_id,
+					// 	difference_qty,
+					// 	percentage_progress,
 					// },
 				});
 			} else {
@@ -220,16 +350,62 @@ exports.accuracyTest = async (req, res) => {
 	}
 };
 
-exports.addPsikotest = async (req, res) => {
+exports.enduranceTestAccumulation = async (req, res) => {
 	try {
-		const { name, category_test_id, test_code, open, close, quota, description, instruction } = req.body;
-		const psikotest = await Psikotest.create({ name, category_test_id, test_code, open, close, quota, description, instruction });
-		res.status(200).json({
-			status: "success",
-			data: {
-				psikotest,
-			},
-		});
+		if (req.body.test_code && req.body.access_code && req.body.user_id) {
+			const { test_code, access_code, user_id } = req.body;
+
+			//check existence test_code and access_code
+			const psikotest = await Psikotest.findOne({
+				where: {
+					test_code: test_code,
+				},
+				include: {
+					model: Access_code,
+					where: {
+						access_code: access_code,
+					},
+				},
+			});
+			if (psikotest !== null) {
+				const psikotest_id = psikotest.id;
+
+				let enduranceTest = await Endurance_prex_grade.findAll({
+					order: [["id", "ASC"]],
+					where: {
+						user_id: Number(user_id),
+						psikotest_id,
+					},
+				});
+
+				let percentage_progress = [];
+				await enduranceTest.forEach((item) => {
+					percentage_progress.push(item.percentage_progress);
+				});
+
+				let average_percentage_progress = percentage_progress.reduce((a, b) => a + b, 0);
+				average_percentage_progress = average_percentage_progress / percentage_progress.length;
+
+				const accumulation_endurance = await Accumulation_endurance.create({ user_id, psikotest_id, percentage_progress: average_percentage_progress });
+
+				res.status(200).json({
+					status: "success",
+					data: {
+						accumulation_endurance,
+					},
+				});
+			} else {
+				res.status(403).json({
+					status: "failed",
+					message: "access_code or test_code isn't valid",
+				});
+			}
+		} else {
+			res.status(403).json({
+				status: "failed",
+				message: "You must provide a test_code, user_id and access_code",
+			});
+		}
 	} catch (error) {
 		console.log(error);
 		res.status(500).json({
@@ -239,19 +415,77 @@ exports.addPsikotest = async (req, res) => {
 	}
 };
 
-exports.editPsikotest = async (req, res) => {
+exports.sendAnswerPsikotest = async (req, res) => {
 	try {
-		const psikotest = await Psikotest.update(req.body, {
-			where: {
-				id: req.params.id,
-			},
-		});
-		res.status(200).json({
-			status: "success",
-			data: {
-				psikotest,
-			},
-		});
+		if (req.body.test_code && req.body.access_code) {
+			const { test_code, access_code, data } = req.body;
+
+			//check existence test_code and access_code
+			const psikotest = await Psikotest.findOne({
+				where: {
+					test_code: test_code,
+				},
+				include: {
+					model: Access_code,
+					where: {
+						access_code: access_code,
+					},
+				},
+			});
+			if (psikotest !== null) {
+				const user_id = Number(req.query.user);
+				const qty_kecepatan = data.length;
+				let qty_ketepatan = 0;
+				let qty_question;
+
+				data.forEach(async (item, i) => {
+					const exam_id = item.exam_id;
+
+					//get qty of question
+					const url = `${process.env.SERVER_URL}/exam/${Number(exam_id)}`;
+					const getQuestionInExam = await axios.get(url);
+					qty_question = getQuestionInExam.data.data.questions.length;
+
+					//a,c,d,e = 0,1,2,3,4
+					const question = await Question.findOne({
+						where: {
+							id: item.question_id,
+						},
+						include: [{ model: Option }, { model: Answer }],
+					});
+					const options = JSON.parse(JSON.stringify(question.Options));
+					const answer = JSON.parse(JSON.stringify(question.Answer));
+
+					//check answer
+					if (options[item.answer].id === Number(answer.option_id)) {
+						qty_ketepatan += 1;
+					}
+
+					//store data student answer
+					await Student_answer.create({ user_id, exam_id: Number(exam_id), question_id: item.question_id, option_id: Number(item.answer) });
+
+					//store data accuracy and speed
+					if (i === data.length - 1) {
+						await Speed_prex_grade.create({ user_id, psikotest_id: psikotest.id, exam_id: Number(exam_id), qty: qty_kecepatan, qty_question });
+						await Accuracy_prex_grade.create({ user_id, psikotest_id: psikotest.id, exam_id: Number(exam_id), qty: qty_ketepatan, qty_question });
+					}
+				});
+				res.status(200).json({
+					status: "success",
+					message: "send answer psikotest success",
+				});
+			} else {
+				res.status(403).json({
+					status: "failed",
+					message: "access_code or test_code isn't valid",
+				});
+			}
+		} else {
+			res.status(403).json({
+				status: "failed",
+				message: "You must provide a test_code, user_id and access_code",
+			});
+		}
 	} catch (error) {
 		console.log(error);
 		res.status(500).json({
--- app/psikotest/router.js
+++ app/psikotest/router.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllPsikotest, speedTest, speedTestAccumulation, accuracyTest } = require("./controller");
+const { getAllPsikotest, speedTest, speedTestAccumulation, accuracyTest, accuracyTestAccumulation, enduranceTest, sendAnswerPsikotest, enduranceTestAccumulation } = require("./controller");
 
 router.get("/psikotes", getAllPsikotest);
 router.post("/psikotes/kecepatan/send-answer", speedTest);
 router.post("/psikotes/kecepatan/calculate", speedTestAccumulation);
 router.post("/psikotes/ketelitian/send-answer", accuracyTest);
+router.post("/psikotes/ketelitian/calculate", accuracyTestAccumulation);
+router.post("/psikotes/ketahanan/send-answer", enduranceTest);
+router.post("/psikotes/ketahanan/calculate", enduranceTestAccumulation);
+router.post("/psikotes/send-answer", sendAnswerPsikotest);
 
 module.exports = router;
